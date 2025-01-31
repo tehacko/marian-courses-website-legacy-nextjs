4958816325a0a612d90afdb1772737f85d2a5f71
Cornelius Lundi <hajek@cmgp.cz>
2025-01-31 18:26:44 +0100

    minor form fixes

--- components/Courses/ImagePicker.js
+++ components/Courses/ImagePicker.js
@@ -17,6 +17,7 @@ export default function ImagePicker({label, name}) {
         const file = event.target.files[0];
 
         if (!file) {
+            setPickedImage(null);
             return;
         }
 
@@ -51,6 +52,7 @@ export default function ImagePicker({label, name}) {
                     name={name}
                     ref={imageInput}
                     onChange={handleImageChange}
+                    required
                 />
                 <button 
                     className={classes.button}
