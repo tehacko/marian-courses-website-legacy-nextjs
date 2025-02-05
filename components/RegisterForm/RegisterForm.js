fca51d3156bea68e5b842d39741e4e5db7d540cc
Cornelius Lundi <hajek@cmgp.cz>
2025-02-05 16:51:44 +0100

    password hash logic commenced

--- components/RegisterForm/RegisterForm.js
+++ components/RegisterForm/RegisterForm.js
@@ -1,26 +1,36 @@
-import Link from 'next/link';
+'use client';
 
-export default function AuthForm() {
+import Link from 'next/link';
+import { useActionState } from 'react';
+import { signup } from '@/lib/server-actions';
+
+export default function RegisterForm() {
+  const [formState, formAction] = useActionState(signup, {});
   return (
-    <form id="auth-form">
+    <form id="auth-form" action={formAction}>
       <div>
         <img src="/images/auth-icon.jpg" alt="A lock icon" />
       </div>
       <p>
-        <label htmlFor="email">Email</label>
+        <label htmlFor="email">Email: </label>
         <input type="email" name="email" id="email" />
       </p>
       <p>
-        <label htmlFor="password">Password</label>
+        <label htmlFor="password">Heslo: </label>
         <input type="password" name="password" id="password" />
       </p>
+      {formState.errors && (<ul id="form-errors">
+        {Object.keys(formState.errors).map((error) => (
+          <li key={error}>{formState.errors[error]}</li>
+          ))}
+        </ul>)}
       <p>
         <button type="submit">
-          Create Account
+          Vytvořit účet
         </button>
       </p>
       <p>
-        <Link href="/">Login with existing account.</Link>
+        <Link href="/">Přihlásit se existujícím účtem.</Link>
       </p>
     </form>
   );
